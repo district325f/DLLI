b5845231f01cd0decde72e9324b8a4856e9b5ce5
district325f <lionsclubkageshwori2014@gmail.com>
2026-08-18 12:57:01 +0530

    Update version to 1.2.0 in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DLLI_CONFIG = {
-  VERSION: '1.1.0',
+  VERSION: '1.2.0',
   API_URL: 'https://script.google.com/macros/s/AKfycbz2l9-UaDT7S8WcbNu9-yg4r-uqo5Eu3pxVLizxB7o2WR9bfLPu8SJoQ9BQsGQkLyFq/exec',
   LOGO_PATH: 'logo.png',
   ADMIN_PAGE: 'admin.html',
